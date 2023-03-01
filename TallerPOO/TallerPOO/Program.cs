d5c131d42ee2e974c1ac61c873bbc0ab6ef9f6a3
Daniel Zapata <DZAPATAR30@GMAIL.COM>
2023-03-01 10:21:02 -0500

    menu

--- TallerPOO/TallerPOO/Program.cs
+++ TallerPOO/TallerPOO/Program.cs
@@ -12,13 +12,81 @@ namespace TallerPOO
             {
                 Console.WriteLine(objPro.MainMenu());
                 option = Convert.ToInt32(Console.ReadLine());
+                Vehicle vehicle;
+                switch (option)
+                {
+                    case 1:
+                        objPro.BuyMotorizedVehicle(vehicle);
+                        break;
+                    case 2:
+                        objPro.BuyAerialVehicle(vehicle);
+                        break;
+                    case 3:
+                        objPro.BuyCargoAerialVehicle(vehicle);
+                        break;
+                    case 4:
+                        objPro.BuyHumanPoweredVehicle(vehicle);
+                        break;
+                    case 5:
+                        objPro.BuyMilitaryAerialVehicle(vehicle);
+                        break;
+                    case 6:
+                        objPro.BuyUsedMotorizedVehicle(vehicle);
+                        break;
+                    case 0:
+                        Console.WriteLine("Goodbye!");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid option. Please select again.");
+                        break;
+                }
+            
+
             } while(option!=0);
         }
 
-        public void Buy()
+        public void BuyMotorizedVehicle(Vehicle vehicle)
         {
 
+
+            vehicle = new MotorizedLandVehicle()
+            {
+
+            }
         }
+
+        public void BuyAerialVehicle(Vehicle vehicle)
+        {
+            // Code to allow the user to purchase an aerial vehicle
+            // ...
+        }
+
+        public void BuyCargoAerialVehicle(Vehicle vehicle)
+        {
+            // Code to allow the user to purchase a cargo aerial vehicle
+            // ...
+        }
+
+        public void BuyHumanPoweredVehicle(Vehicle vehicle)
+        {
+            // Code to allow the user to purchase a human-powered vehicle
+            // ...
+        }
+
+        public void BuyMilitaryAerialVehicle(Vehicle vehicle)
+        {
+            // Code to allow the user to purchase a military aerial vehicle
+            // ...
+        }
+
+        public void BuyUsedMotorizedVehicle(Vehicle vehicle)
+        {
+            // Code to allow the user to purchase a used motorized vehicle
+            // ...
+        }
+
+
+
         public string MainMenu()
         {
 
@@ -31,5 +99,7 @@ namespace TallerPOO
                 "\n6. Buy a used motorized vehicle"+
                 "\n0. Exit";
         }
+
+
     }
 }
